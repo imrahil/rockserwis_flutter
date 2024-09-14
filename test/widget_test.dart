8c3505731678e4346c068d9cf330b986c5a390b8
Jarek Szczepanski <imrahil@imrahil.com>
2024-09-14 13:17:15 +0200

    Test: Introduce widget tests for app initialization
    Adds widget tests to verify the correct setup of the MaterialApp widget, including its theme configuration and the use of LoginPage as the initial screen. This ensures a robust and visually consistent user experience from the outset.

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -1,29 +1,62 @@
-// This is a basic Flutter widget test.
-//
-// To perform an interaction with a widget in your test, use the WidgetTester
-// utility in the flutter_test package. For example, you can send tap and scroll
-// gestures. You can also use WidgetTester to find child widgets in the widget
-// tree, read text, and verify that the values of widget properties are correct.
-
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
+import 'package:provider/provider.dart';
+import 'package:rockserwis_podcaster/api/api.dart';
 import 'package:rockserwis_podcaster/main.dart';
+import 'package:rockserwis_podcaster/screens/login.dart';
+import 'package:mockito/mockito.dart';
+
+class MockAPI extends Mock implements API {}
 
 void main() {
-  testWidgets('Counter increments smoke test', (WidgetTester tester) async {
-    // Build our app and trigger a frame.
-    await tester.pumpWidget(MusicPlayer());
+  testWidgets('MusicPlayer builds MaterialApp with correct theme',
+      (WidgetTester tester) async {
+    await tester.pumpWidget(
+      MultiProvider(
+        providers: [
+          Provider<API>(create: (_) => MockAPI()),
+        ],
+        child: const MusicPlayer(),
+      ),
+    );
 
-    // Verify that our counter starts at 0.
-    expect(find.text('0'), findsOneWidget);
-    expect(find.text('1'), findsNothing);
+    // Verify MaterialApp is created
+    expect(find.byType(MaterialApp), findsOneWidget);
 
-    // Tap the '+' icon and trigger a frame.
-    await tester.tap(find.byIcon(Icons.add));
-    await tester.pump();
+    // Verify MaterialApp's title
+    final MaterialApp app =
+        tester.widget<MaterialApp>(find.byType(MaterialApp));
+    expect(app.title, 'Rockserwis.fm Podcast Player');
 
-    // Verify that our counter has incremented.
-    expect(find.text('0'), findsNothing);
-    expect(find.text('1'), findsOneWidget);
+    // Verify MaterialApp's debugShowCheckedModeBanner is false
+    expect(app.debugShowCheckedModeBanner, false);
+
+    // Verify ThemeData's brightness is Brightness.dark
+    expect(app.theme?.brightness, Brightness.dark);
+
+    // Verify InputDecorationTheme properties
+    expect(app.theme?.inputDecorationTheme.filled, true);
+    expect(app.theme?.inputDecorationTheme.fillColor, Colors.grey[850]);
+    expect(app.theme?.inputDecorationTheme.labelStyle!.color, Colors.grey[400]);
+    expect(app.theme?.inputDecorationTheme.border!.borderSide.color,
+        Colors.grey[600]);
+    expect(app.theme?.inputDecorationTheme.enabledBorder!.borderSide.color,
+        Colors.grey[600]);
+    expect(app.theme?.inputDecorationTheme.focusedBorder!.borderSide.color,
+        Colors.blue);
+  });
+
+  testWidgets('MusicPlayer has LoginPage as home', (WidgetTester tester) async {
+    await tester.pumpWidget(
+      MultiProvider(
+        providers: [
+          Provider<API>(create: (_) => MockAPI()),
+        ],
+        child: const MusicPlayer(),
+      ),
+    );
+
+    // Verify LoginPage is the home
+    expect(find.byType(LoginPage), findsOneWidget);
   });
 }
