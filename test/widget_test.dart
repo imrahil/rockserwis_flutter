f9f618c3eeb29591d0cd9ecd4b23c6008837b74c
Jarek Szczepanski <imrahil@imrahil.com>
2024-09-23 15:08:10 +0200

    fix: fixed tests

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -5,16 +5,21 @@ import 'package:rockserwis_podcaster/api/api.dart';
 import 'package:rockserwis_podcaster/main.dart';
 import 'package:rockserwis_podcaster/screens/login.dart';
 import 'package:mockito/mockito.dart';
+import 'package:rockserwis_podcaster/theme.dart';
 
 class MockAPI extends Mock implements API {}
 
 void main() {
   testWidgets('MusicPlayer builds MaterialApp with correct theme',
       (WidgetTester tester) async {
+    // Create a ThemeProvider and set it to dark mode
+    final themeProvider = ThemeProvider();
+
     await tester.pumpWidget(
       MultiProvider(
         providers: [
           Provider<API>(create: (_) => MockAPI()),
+          ChangeNotifierProvider<ThemeProvider>.value(value: themeProvider),
         ],
         child: const MusicPlayer(homeComp: LoginPage()),
       ),
@@ -23,18 +28,12 @@ void main() {
     // Verify MaterialApp is created
     expect(find.byType(MaterialApp), findsOneWidget);
 
-    // Verify MaterialApp's title
-    final MaterialApp app =
-        tester.widget<MaterialApp>(find.byType(MaterialApp));
+    final app = tester.widget<MaterialApp>(find.byType(MaterialApp));
+
     expect(app.title, 'Rockserwis.fm Podcast Player');
-
-    // Verify MaterialApp's debugShowCheckedModeBanner is false
     expect(app.debugShowCheckedModeBanner, false);
-
-    // Verify ThemeData's brightness is Brightness.dark
     expect(app.theme?.brightness, Brightness.dark);
 
-    // Verify InputDecorationTheme properties
     expect(app.theme?.inputDecorationTheme.filled, true);
     expect(app.theme?.inputDecorationTheme.fillColor, Colors.grey[850]);
     expect(app.theme?.inputDecorationTheme.labelStyle!.color, Colors.grey[400]);
@@ -46,11 +45,47 @@ void main() {
         Colors.blue);
   });
 
-  testWidgets('MusicPlayer has LoginPage as home', (WidgetTester tester) async {
+  // Add a similar test for light mode
+  testWidgets('MusicPlayer builds MaterialApp with correct light theme',
+      (WidgetTester tester) async {
+    final themeProvider = ThemeProvider();
+    themeProvider.toggleTheme();
+
     await tester.pumpWidget(
       MultiProvider(
         providers: [
           Provider<API>(create: (_) => MockAPI()),
+          ChangeNotifierProvider<ThemeProvider>.value(value: themeProvider),
+        ],
+        child: const MusicPlayer(homeComp: LoginPage()),
+      ),
+    );
+
+    // Verify MaterialApp is created
+    expect(find.byType(MaterialApp), findsOneWidget);
+
+    final app = tester.widget<MaterialApp>(find.byType(MaterialApp));
+
+    expect(app.theme?.brightness, Brightness.light);
+    expect(app.theme?.inputDecorationTheme.filled, true);
+    expect(app.theme?.inputDecorationTheme.fillColor, Colors.grey[200]);
+    expect(app.theme?.inputDecorationTheme.labelStyle!.color, Colors.grey[700]);
+    expect(app.theme?.inputDecorationTheme.border!.borderSide.color,
+        Colors.grey[400]);
+    expect(app.theme?.inputDecorationTheme.enabledBorder!.borderSide.color,
+        Colors.grey[400]);
+    expect(app.theme?.inputDecorationTheme.focusedBorder!.borderSide.color,
+        Colors.blue);
+  });
+
+  testWidgets('MusicPlayer has LoginPage as home', (WidgetTester tester) async {
+    final themeProvider = ThemeProvider();
+
+    await tester.pumpWidget(
+      MultiProvider(
+        providers: [
+          Provider<API>(create: (_) => MockAPI()),
+          ChangeNotifierProvider<ThemeProvider>.value(value: themeProvider),
         ],
         child: const MusicPlayer(
           homeComp: LoginPage(),
