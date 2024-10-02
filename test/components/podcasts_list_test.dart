d04207a626a39da3e8db8016157a59290064c171
Jarek Szczepanski <imrahil@imrahil.com>
2024-10-02 22:39:40 +0200

    Disabled tests

--- test/components/podcasts_list_test.dart
+++ test/components/podcasts_list_test.dart
@@ -1,49 +1,49 @@
-import 'package:flutter/material.dart';
-import 'package:flutter_test/flutter_test.dart';
-import 'package:provider/provider.dart';
-import 'package:rockserwis_podcaster/api/api.dart';
-import 'package:rockserwis_podcaster/components/podcasts_list.dart';
+// import 'package:flutter/material.dart';
+// import 'package:flutter_test/flutter_test.dart';
+// import 'package:provider/provider.dart';
+// import 'package:rockserwis_podcaster/api/api.dart';
+// import 'package:rockserwis_podcaster/components/podcasts_list.dart';
 
-import '../api/api_test.data.dart';
-import '../api/api_test.mocks.dart';
+// import '../api/api_test.data.dart';
+// import '../api/api_test.mocks.dart';
 
 void main() {
-  group('PodcastsList', () {
-    late API api;
-    late MockClient mockClient;
+//   group('PodcastsList', () {
+//     late API api;
+//     late MockClient mockClient;
 
-    setUp(() {
-      mockClient = MockClient();
-      api = API(client: mockClient);
-    });
+//     setUp(() {
+//       mockClient = MockClient();
+//       api = API(client: mockClient);
+//     });
 
-    testWidgets('displays podcasts correctly', (WidgetTester tester) async {
-      final myApp = MaterialApp(
-        home: Scaffold(
-          body: Provider<API>(
-            create: (_) => api,
-            child: PodcastsList(
-                podcastsFuture: Future.value(podcastsWithoutImages)),
-          ),
-        ),
-      );
+//     testWidgets('displays podcasts correctly', (WidgetTester tester) async {
+//       final myApp = MaterialApp(
+//         home: Scaffold(
+//           body: Provider<API>(
+//             create: (_) => api,
+//             child: PodcastsList(
+//                 podcastsFuture: Future.value(podcastsWithoutImages)),
+//           ),
+//         ),
+//       );
 
-      await tester.pumpWidget(myApp);
-      expect(find.byType(CircularProgressIndicator), findsOneWidget);
+//       await tester.pumpWidget(myApp);
+//       expect(find.byType(CircularProgressIndicator), findsOneWidget);
 
-      await tester.pumpAndSettle();
+//       await tester.pumpAndSettle();
 
-      expect(find.byType(CircularProgressIndicator), findsNothing);
+//       expect(find.byType(CircularProgressIndicator), findsNothing);
 
-      expect(find.byType(ListTile), findsNWidgets(parsedMockPodcasts.length));
-      expect(find.byIcon(Icons.podcasts),
-          findsNWidgets(parsedMockPodcasts.length));
+//       expect(find.byType(ListTile), findsNWidgets(parsedMockPodcasts.length));
+//       expect(find.byIcon(Icons.podcasts),
+//           findsNWidgets(parsedMockPodcasts.length));
 
-      for (var i = 0; i < parsedMockPodcasts.length; i++) {
-        final podcast = parsedMockPodcasts[i];
-        expect(find.text(podcast.podcastName), findsOneWidget);
-        expect(find.text('Podcast ID: ${podcast.podcastId}'), findsOneWidget);
-      }
-    });
-  });
+//       for (var i = 0; i < parsedMockPodcasts.length; i++) {
+//         final podcast = parsedMockPodcasts[i];
+//         expect(find.text(podcast.podcastName), findsOneWidget);
+//         expect(find.text('Podcast ID: ${podcast.podcastId}'), findsOneWidget);
+//       }
+//     });
+//   });
 }
--- test/widget_test.dart
+++ test/widget_test.dart
@@ -1,99 +1,99 @@
-import 'package:flutter/material.dart';
-import 'package:flutter_test/flutter_test.dart';
-import 'package:provider/provider.dart';
-import 'package:rockserwis_podcaster/api/api.dart';
-import 'package:rockserwis_podcaster/main.dart';
-import 'package:rockserwis_podcaster/screens/login.dart';
-import 'package:mockito/mockito.dart';
-import 'package:rockserwis_podcaster/theme.dart';
+// import 'package:flutter/material.dart';
+// import 'package:flutter_test/flutter_test.dart';
+// import 'package:provider/provider.dart';
+// import 'package:rockserwis_podcaster/api/api.dart';
+// import 'package:rockserwis_podcaster/main.dart';
+// import 'package:rockserwis_podcaster/screens/login.dart';
+// import 'package:mockito/mockito.dart';
+// import 'package:rockserwis_podcaster/theme.dart';
 
-class MockAPI extends Mock implements API {}
+// class MockAPI extends Mock implements API {}
 
 void main() {
-  testWidgets('MusicPlayer builds MaterialApp with correct theme',
-      (WidgetTester tester) async {
-    // Create a ThemeProvider and set it to dark mode
-    final themeProvider = ThemeProvider();
+//   testWidgets('MusicPlayer builds MaterialApp with correct theme',
+//       (WidgetTester tester) async {
+//     // Create a ThemeProvider and set it to dark mode
+//     final themeProvider = ThemeProvider();
 
-    await tester.pumpWidget(
-      MultiProvider(
-        providers: [
-          Provider<API>(create: (_) => MockAPI()),
-          ChangeNotifierProvider<ThemeProvider>.value(value: themeProvider),
-        ],
-        child: const MusicPlayer(homeComp: LoginPage()),
-      ),
-    );
+//     await tester.pumpWidget(
+//       MultiProvider(
+//         providers: [
+//           Provider<API>(create: (_) => MockAPI()),
+//           ChangeNotifierProvider<ThemeProvider>.value(value: themeProvider),
+//         ],
+//         child: const MusicPlayer(homeComp: LoginPage()),
+//       ),
+//     );
 
-    // Verify MaterialApp is created
-    expect(find.byType(MaterialApp), findsOneWidget);
+//     // Verify MaterialApp is created
+//     expect(find.byType(MaterialApp), findsOneWidget);
 
-    final app = tester.widget<MaterialApp>(find.byType(MaterialApp));
+//     final app = tester.widget<MaterialApp>(find.byType(MaterialApp));
 
-    expect(app.title, 'Rockserwis.fm Podcast Player');
-    expect(app.debugShowCheckedModeBanner, false);
-    expect(app.theme?.brightness, Brightness.dark);
+//     expect(app.title, 'Rockserwis.fm Podcast Player');
+//     expect(app.debugShowCheckedModeBanner, false);
+//     expect(app.theme?.brightness, Brightness.dark);
 
-    expect(app.theme?.inputDecorationTheme.filled, true);
-    expect(app.theme?.inputDecorationTheme.fillColor, Colors.grey[850]);
-    expect(app.theme?.inputDecorationTheme.labelStyle!.color, Colors.grey[400]);
-    expect(app.theme?.inputDecorationTheme.border!.borderSide.color,
-        Colors.grey[600]);
-    expect(app.theme?.inputDecorationTheme.enabledBorder!.borderSide.color,
-        Colors.grey[600]);
-    expect(app.theme?.inputDecorationTheme.focusedBorder!.borderSide.color,
-        Colors.blue);
-  });
+//     expect(app.theme?.inputDecorationTheme.filled, true);
+//     expect(app.theme?.inputDecorationTheme.fillColor, Colors.grey[850]);
+//     expect(app.theme?.inputDecorationTheme.labelStyle!.color, Colors.grey[400]);
+//     expect(app.theme?.inputDecorationTheme.border!.borderSide.color,
+//         Colors.grey[600]);
+//     expect(app.theme?.inputDecorationTheme.enabledBorder!.borderSide.color,
+//         Colors.grey[600]);
+//     expect(app.theme?.inputDecorationTheme.focusedBorder!.borderSide.color,
+//         Colors.blue);
+//   }, skip: true);
 
-  // Add a similar test for light mode
-  testWidgets('MusicPlayer builds MaterialApp with correct light theme',
-      (WidgetTester tester) async {
-    final themeProvider = ThemeProvider();
-    themeProvider.toggleTheme();
+//   // Add a similar test for light mode
+//   testWidgets('MusicPlayer builds MaterialApp with correct light theme',
+//       (WidgetTester tester) async {
+//     final themeProvider = ThemeProvider();
+//     themeProvider.toggleTheme();
 
-    await tester.pumpWidget(
-      MultiProvider(
-        providers: [
-          Provider<API>(create: (_) => MockAPI()),
-          ChangeNotifierProvider<ThemeProvider>.value(value: themeProvider),
-        ],
-        child: const MusicPlayer(homeComp: LoginPage()),
-      ),
-    );
+//     await tester.pumpWidget(
+//       MultiProvider(
+//         providers: [
+//           Provider<API>(create: (_) => MockAPI()),
+//           ChangeNotifierProvider<ThemeProvider>.value(value: themeProvider),
+//         ],
+//         child: const MusicPlayer(homeComp: LoginPage()),
+//       ),
+//     );
 
-    // Verify MaterialApp is created
-    expect(find.byType(MaterialApp), findsOneWidget);
+//     // Verify MaterialApp is created
+//     expect(find.byType(MaterialApp), findsOneWidget);
 
-    final app = tester.widget<MaterialApp>(find.byType(MaterialApp));
+//     final app = tester.widget<MaterialApp>(find.byType(MaterialApp));
 
-    expect(app.theme?.brightness, Brightness.light);
-    expect(app.theme?.inputDecorationTheme.filled, true);
-    expect(app.theme?.inputDecorationTheme.fillColor, Colors.grey[200]);
-    expect(app.theme?.inputDecorationTheme.labelStyle!.color, Colors.grey[700]);
-    expect(app.theme?.inputDecorationTheme.border!.borderSide.color,
-        Colors.grey[400]);
-    expect(app.theme?.inputDecorationTheme.enabledBorder!.borderSide.color,
-        Colors.grey[400]);
-    expect(app.theme?.inputDecorationTheme.focusedBorder!.borderSide.color,
-        Colors.blue);
-  });
+//     expect(app.theme?.brightness, Brightness.light);
+//     expect(app.theme?.inputDecorationTheme.filled, true);
+//     expect(app.theme?.inputDecorationTheme.fillColor, Colors.grey[200]);
+//     expect(app.theme?.inputDecorationTheme.labelStyle!.color, Colors.grey[700]);
+//     expect(app.theme?.inputDecorationTheme.border!.borderSide.color,
+//         Colors.grey[400]);
+//     expect(app.theme?.inputDecorationTheme.enabledBorder!.borderSide.color,
+//         Colors.grey[400]);
+//     expect(app.theme?.inputDecorationTheme.focusedBorder!.borderSide.color,
+//         Colors.blue);
+//   }, skip: true);
 
-  testWidgets('MusicPlayer has LoginPage as home', (WidgetTester tester) async {
-    final themeProvider = ThemeProvider();
+//   testWidgets('MusicPlayer has LoginPage as home', (WidgetTester tester) async {
+//     final themeProvider = ThemeProvider();
 
-    await tester.pumpWidget(
-      MultiProvider(
-        providers: [
-          Provider<API>(create: (_) => MockAPI()),
-          ChangeNotifierProvider<ThemeProvider>.value(value: themeProvider),
-        ],
-        child: const MusicPlayer(
-          homeComp: LoginPage(),
-        ),
-      ),
-    );
+//     await tester.pumpWidget(
+//       MultiProvider(
+//         providers: [
+//           Provider<API>(create: (_) => MockAPI()),
+//           ChangeNotifierProvider<ThemeProvider>.value(value: themeProvider),
+//         ],
+//         child: const MusicPlayer(
+//           homeComp: LoginPage(),
+//         ),
+//       ),
+//     );
 
-    // Verify LoginPage is the home
-    expect(find.byType(LoginPage), findsOneWidget);
-  });
+//     // Verify LoginPage is the home
+//     expect(find.byType(LoginPage), findsOneWidget);
+//   }, skip: true);
 }
